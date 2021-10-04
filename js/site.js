5354ff951e814c823022d8f04552f9cfaffc406e
Bobby Davis <bdavis@core-techs.net>
2021-10-04 17:10:26 -0400

    init committ
    version 1.0

---
+++ js/site.js
@@ -0,0 +1,12 @@
+// displays a message to the user
+function displayMessage(){
+    //declare our variable
+    let msg = "" ;    
+    
+    //pull the valeu from the input box
+    msg = document.getElementById("message").value;    
+    
+    //alert the user
+    alert(msg);
+}
+
